fbb3b02a6c692d57306355ad2fa3ef20c16a77be
FernandoH94 <fernando.hsousa@senacsp.edu.br>
2025-02-21 19:27:02 -0300

    Adicionar arquivos de projeto.

---
+++ WFNovo/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace WFNovo
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblNome = new Label();
+            btnCliqueAqui = new Button();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            SuspendLayout();
+            // 
+            // lblNome
+            // 
+            lblNome.AutoSize = true;
+            lblNome.Font = new Font("Showcard Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblNome.Location = new Point(37, 46);
+            lblNome.Name = "lblNome";
+            lblNome.Size = new Size(99, 20);
+            lblNome.TabIndex = 0;
+            lblNome.Text = "Username:";
+            lblNome.Click += lblNome_Click;
+            // 
+            // btnCliqueAqui
+            // 
+            btnCliqueAqui.Location = new Point(169, 128);
+            btnCliqueAqui.Name = "btnCliqueAqui";
+            btnCliqueAqui.Size = new Size(75, 23);
+            btnCliqueAqui.TabIndex = 1;
+            btnCliqueAqui.Text = "Login";
+            btnCliqueAqui.UseVisualStyleBackColor = true;
+            btnCliqueAqui.Click += button1_Click;
+            btnCliqueAqui.MouseClick += btnCliqueAqui_MouseClick;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Showcard Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(37, 91);
+            label1.Name = "label1";
+            label1.Size = new Size(105, 20);
+            label1.TabIndex = 2;
+            label1.Text = "Password:";
+            label1.Click += label1_Click_1;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(160, 43);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(139, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(160, 88);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(139, 23);
+            textBox2.TabIndex = 4;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(348, 194);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(btnCliqueAqui);
+            Controls.Add(lblNome);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Login";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblNome;
+        private Button btnCliqueAqui;
+        private Label label1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+    }
+}
